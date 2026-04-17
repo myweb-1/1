880e29ff6be3dee9978f4ae63e431124d69e841a
myweb-1 <myweb1681@gmail.com>
2026-04-17 10:33:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// LOGO ANIMATION
-const texts = [
-"Create Digital Experiences",
-"A5hraf Studio",
-"Design. Build. Grow."
-];
-
-let i = 0;
-const el = document.querySelector(".logo-text");
-
-function animate(){
-el.style.opacity = 0;
-
-setTimeout(() => {
-el.innerText = texts[i];
-el.style.opacity = 1;
-
-i = (i + 1) % texts.length;
-}, 400);
-}
-
-animate();
-setInterval(animate, 2500);
-
-// WHATSAPP ORDER
 document.addEventListener("DOMContentLoaded", function(){
 
 document.getElementById("orderForm").addEventListener("submit", function(e){
