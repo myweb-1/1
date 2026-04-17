6c1a86b689acb254d994e6c6b727fd6d202829a3
myweb-1 <myweb1681@gmail.com>
2026-04-17 09:44:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let lang = "en";
 
-// translate
+// translation
 function toggleLang(){
 if(lang === "en"){
 document.getElementById("aboutTitle").innerText = "من أنا";
 document.getElementById("aboutText").innerText =
-"أنا فريلانسر متخصص في تصميم المواقع، اللوجو، المونتاج، وتصميم QR.";
+"أنا فريلانسر متخصص في التصميم الرقمي الحديث.";
 lang = "ar";
 }
 else{
 document.getElementById("aboutTitle").innerText = "About Me";
 document.getElementById("aboutText").innerText =
-"I am a freelance designer specialized in modern web design, logo creation, video editing, and QR design.";
+"I am a freelance designer specialized in modern digital design.";
 lang = "en";
 }
 }
 
-// whatsapp order
+// WhatsApp order
 document.addEventListener("DOMContentLoaded", function(){
 
 document.getElementById("orderForm").addEventListener("submit", function(e){
@@ -27,7 +27,7 @@ let service = document.getElementById("service").value;
 let desc = document.getElementById("desc").value;
 
 let message =
-`🔥 New Project Request
+`🔥 New Order Request
 👤 Name: ${name}
 🛠 Service: ${service}
 📝 Description: ${desc}`;
@@ -37,6 +37,7 @@ let phone = "201080403165";
 let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
 
 window.open(url, "_blank");
+
 });
 
 });
