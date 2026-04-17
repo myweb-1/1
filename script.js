abb249866dc297851ea9c3139b2872589e97d464
myweb-1 <myweb1681@gmail.com>
2026-04-17 10:25:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function scrollToAbout(){
-document.getElementById("about").scrollIntoView({
-behavior:"smooth"
-});
+// LOGO ANIMATION
+const texts = [
+"Create Digital Experiences",
+"A5hraf Studio",
+"Design. Build. Grow."
+];
+
+let i = 0;
+const el = document.querySelector(".logo-text");
+
+function animate(){
+el.style.opacity = 0;
+
+setTimeout(() => {
+el.innerText = texts[i];
+el.style.opacity = 1;
+
+i = (i + 1) % texts.length;
+}, 400);
 }
 
-// WhatsApp
+animate();
+setInterval(animate, 2500);
+
+// WHATSAPP ORDER
 document.addEventListener("DOMContentLoaded", function(){
 
 document.getElementById("orderForm").addEventListener("submit", function(e){
@@ -14,15 +32,15 @@ let name = document.getElementById("name").value;
 let service = document.getElementById("service").value;
 let desc = document.getElementById("desc").value;
 
-let message =
+let msg =
 `New Order:
 Name: ${name}
 Service: ${service}
-Description: ${desc}`;
+Details: ${desc}`;
 
 let phone = "201080403165";
 
-window.open("https://wa.me/" + phone + "?text=" + encodeURIComponent(message), "_blank");
+window.open("https://wa.me/" + phone + "?text=" + encodeURIComponent(msg), "_blank");
 
 });
 
