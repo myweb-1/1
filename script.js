2bffb9fd48e99eacd936a9904b30754677506451
myweb-1 <myweb1681@gmail.com>
2026-04-17 09:18:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
+// تغيير اللغة (اختياري تسيبه)
 let lang = "en";
 
-// تغيير اللغة
 function toggleLang(){
 if(lang === "en"){
 document.getElementById("aboutTitle").innerText = "من أنا";
 document.getElementById("aboutText").innerText =
 "أنا فريلانسر متخصص في تصميم المواقع، اللوجو، المونتاج، وتصميم QR.";
-document.getElementById("servicesTitle").innerText = "الخدمات";
 lang = "ar";
 }
 else{
 document.getElementById("aboutTitle").innerText = "About Me";
 document.getElementById("aboutText").innerText =
 "I am a freelance designer specialized in web design, logo design, video editing, QR design.";
-document.getElementById("servicesTitle").innerText = "Services";
 lang = "en";
 }
 }
 
 // إرسال واتساب مباشر
+document.addEventListener("DOMContentLoaded", function(){
+
 document.getElementById("orderForm").addEventListener("submit", function(e){
 e.preventDefault();
 
@@ -27,7 +27,7 @@ let service = document.getElementById("service").value;
 let desc = document.getElementById("desc").value;
 
 let message =
-`📌 New Order:
+`📌 New Order
 👤 Name: ${name}
 🛠 Service: ${service}
 📝 Description: ${desc}`;
@@ -37,4 +37,6 @@ let phone = "201080403165";
 let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
 
 window.open(url, "_blank");
+});
+
 });
